73fd54a86206c82e263ed4ebb7f2abfe79dd2d44
Jamie Twells <jamie@jamietwells.com>
2023-12-22 12:52:40 +0000

    Fixing some whitespace bugs

--- index.js
+++ index.js
@@ -32,13 +32,15 @@ const RegularText = noneOf('\r\n<').atLeast(1).map((chars) => ({ type: 'Regular 
 
 const ParmeterizedLine = seq(Parameter.or(RegularText).many(), end);
 
-const Comment = seq(whitespace.many(), string('#'), whitespace.many(), RestOfLine).desc('comment').map((r) => ({ type: 'Comment', comment: r[3] }));
+const HorizontalWhitespace = regexp(/[^\S\r\n]/).desc(`horizontal whitespace`);
 
-const BlankLine = seq(regexp(/[^\S\r\n]/).many(), newline).desc('blank line').map(_ => ({ type: 'BlankLine' }));
+const Comment = seq(whitespace.many(), string('#'), HorizontalWhitespace.many(), RestOfLine).desc('comment').map((r) => ({ type: 'Comment', comment: r[3] }));
+
+const BlankLine = seq(HorizontalWhitespace.many(), newline).desc('blank line').map(_ => ({ type: 'BlankLine' }));
 
 const Indentation = level => string('  ').desc('indentation').times(level);
 
-const Step = level => word => seq(Indentation(level), string(word), whitespace.many(), ParmeterizedLine).map(([_1, _2, _3, title]) => ({ type: `Step`, word, title }));
+const Step = level => word => seq(Indentation(level), string(word), HorizontalWhitespace.many(), ParmeterizedLine).map(([_1, _2, _3, title]) => ({ type: `Step`, word, title }));
 
 const AnyStep = level => alt(...stepIdentifiers.map(Step(level)));
 
@@ -46,12 +48,17 @@ const DescriptionStop = alt(...keywords.map(w => string(w)));
 
 const DescriptionParser = level => seq(Indentation(level + 1).notFollowedBy(DescriptionStop), RestOfLine).many().map(r => r.map(l => l[1]).join("\n"))
 
-const TitleLine = name => level => seq(Indentation(level), string(name).desc(name), string(':'), whitespace.many(), ParmeterizedLine, DescriptionParser(level))
+const TitleLine = name => level => seq(Indentation(level), string(name).desc(name), string(':'), HorizontalWhitespace.many(), ParmeterizedLine, DescriptionParser(level))
     .map(([_1, _2, _3, _4, title, description]) => ({ type: name, title, description }));
 
 const [ScenarioOutlineTitle, BackgroundTitle, ScenarioTitle, FeatureTitle] = [`Scenario Outline`, `Background`, `Scenario`, `Feature`].map(TitleLine);
 
 const BlankLinesOrComments = [Comment, BlankLine];
+
+const ExampleTitle = level => seq(Indentation(level), string(`Example`).desc(`Example`), string(':'), HorizontalWhitespace.many())
+    .map(() => ({ type: `Example` }));
+
+const ExampleBlock = level => seq(ExampleTitle(level), alt(AnyStep(level + 1), ...BlankLinesOrComments).many()).map(([title, steps]) => ({ ...title, steps }));
 
 const BackgroundBlock = level => seq(BackgroundTitle(level), alt(AnyStep(level + 1), ...BlankLinesOrComments).many()).map(([title, steps]) => ({ ...title, steps }));
 
@@ -68,7 +75,7 @@ const GherkinParser = seq(alt(...BlankLinesOrComments, ScenarioBlock(0), Feature
 async function parseGherkin(name) {
     function printErrorMessage(error, textSample) {
         // Destructure error object
-        const { status, index, expected } = error;
+        const { index, expected } = error;
         const { offset, line, column } = index;
     
         // Split text sample into lines
